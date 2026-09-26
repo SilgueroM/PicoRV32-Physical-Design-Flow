a1cac5a179abe54caa61d4d8318b82145e10e645
msilguer <misilguero2005@gmail.com>
2026-09-26 13:10:39 -0400

    more edits to fc_flow.tcl

--- scripts/fc_flow.tcl
+++ scripts/fc_flow.tcl
@@ -1,13 +1,12 @@
 # 1. Setup Libraries
 set tech_lib "/apps/share64/rocky8/freepdk/freepdk45-1.4/FreePDK45/osu_soc/lib/files/gscl45nm.db"
-set phys_lef "/apps/share64/rocky8/freepdk/freepdk45-1.4/FreePDK45/osu_soc/lib/files/gscl45nm.lef"
+set phys_ndm "freepdk45.ndm"
 
 set target_library $tech_lib
 set link_library "* $tech_lib"
 
-# Create the workspace and read the physical LEF shapes
-create_lib workspace_rv32
-read_lef $phys_lef
+# Create the Fusion Compiler workspace using the compiled NDM
+create_lib workspace_rv32 -ref_libs $phys_ndm
 
 # 2. Read RTL & Synthesize
 read_verilog ../rtl/picorv32.v
