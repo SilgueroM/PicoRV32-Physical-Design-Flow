611114609fad02a8db3398bece67e23efd28c4de
msilguer <misilguero2005@gmail.com>
2026-09-26 13:47:14 -0400

    hopefully this works

--- scripts/fc_flow.tcl
+++ scripts/fc_flow.tcl
@@ -5,12 +5,9 @@ set phys_lef "/apps/share64/rocky8/freepdk/freepdk45-1.4/FreePDK45/osu_soc/lib/f
 set target_library $tech_db
 set link_library "* $tech_db"
 
-# Safely close current block and remove existing library if it's open/cached
-current_block -q
-close_block -q
-if {[file exists workspace_rv32] || [get_libs -quiet workspace_rv32] ne ""} {
-    remove_lib -force workspace_rv32
-}
+# Safely clear out any previously loaded workspace in memory
+catch { close_block -save no }
+catch { remove_lib workspace_rv32 }
 
 # Create design library and read physical LEF
 create_lib workspace_rv32
